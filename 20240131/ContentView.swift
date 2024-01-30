9abfdd3ff311273c83c0481bf683990fdcfee66e
samuel-xiangpeng <samuel_xiangpeng@163.com>
2024-01-30 22:11:00 +0800

    Initial Commit

---
+++ 20240131/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  20240131
+//
+//  Created by 周祥鹏 on 2024/1/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 20240131/_0240131App.swift
@@ -0,0 +1,17 @@
+//
+//  _0240131App.swift
+//  20240131
+//
+//  Created by 周祥鹏 on 2024/1/30.
+//
+
+import SwiftUI
+
+@main
+struct _0240131App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
